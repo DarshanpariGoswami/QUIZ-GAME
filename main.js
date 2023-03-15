1371c556409c7bfb57c133062513aaf969dfb2d9
DarshanpariGoswami <124126426+DarshanpariGoswami@users.noreply.github.com>
2023-03-15 17:55:11 +0530

    js updated

--- main.js
+++ main.js
@@ -1,36 +1,66 @@
-// Questions will be asked
-const Questions = [
+const quastions = [
   {
-    id: 0,
-    q: "What is capital of India?",
-    a: [
-      { text: "gandhinagar", isCorrect: false },
-      { text: "Surat", isCorrect: false },
-      { text: "Delhi", isCorrect: true },
-      { text: "mumbai", isCorrect: false },
+    optioncontainer: "What is capital of India?",
+    OPT: [
+      {
+        a: "gandhinagar",
+        b: "Surat",
+        c: "mumbai",
+        d: "panjab",
+      },
     ],
-  },
-  {
-    id: 1,
-    q: "What is the capital of Thailand?",
-    a: [
-      { text: "Lampang", isCorrect: false, isSelected: false },
-      { text: "phuket", isCorrect: false },
-      { text: "Ayutthaya", isCorrect: false },
-      { text: "Bangkok", isCorrect: true },
+    optioncontainer: "What is the capital of Thailand?",
+    OPT: [
+      {
+        a: "Lampang",
+        b: "phuket",
+        c: "Ayutthaya",
+        d: "Bangkok",
+      },
     ],
-  },
-  {
-    id: 2,
-    q: "What is the capital of Gujarat",
-    a: [
-      { text: "surat", isCorrect: false },
-      { text: "vadodara", isCorrect: false },
-      { text: "gandhinagar", isCorrect: true },
-      { text: "rajkot", isCorrect: false },
+
+    optioncontainer: "What is the capital of Gujarat",
+    OPT: [
+      {
+        a: "surat",
+        b: "vadodara",
+        c: "gandhinagar",
+        d: "rajkot",
+      },
     ],
   },
 ];
 
-// Set start
-var start = true;
+var text = "What is capital of India?";
+const op2 = document.GetElementById("op2");
+const op3 = document.GetElementById("op3");
+const op4 = document.GetElementById("op4");
+const select = optioncontainer[0].OPT(0);
+
+const op1 = document.GetElementById("op1");
+op1.innerHTML = op1.innerText = Questions[id];
+op1.value = Questions[id].a[0].isCorrect;
+
+op1.addEventListener("click", () => {
+  op1.style.backgroundColor = "yellow";
+  op2.style.backgroundColor = "lightskyblue";
+  op3.style.backgroundColor = "lightskyblue";
+  op4.style.backgroundColor = "lightskyblue";
+  select = op1.value;
+});
+
+const result = document.getElementsByName("result");
+result[0].innerText = "";
+const submit = document.GetElementById("submit");
+
+submit[0].AddEventListener("click", () => {
+  if (select == "true") {
+    result[0].innerHTML = "true";
+    result[0].style.color = "red";
+  } else {
+    result.innerHTML = "false";
+    result[0].style.color = "red";
+  }
+});
+
+//const op = document.GetElementById("submit");
